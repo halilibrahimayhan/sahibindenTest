31d89615e1c3ab4871d413e3b5c5e2ef9c76472f
halilibrahimayhan <halilibrahimayhan@gmail.com>
2022-02-07 10:14:27 +0300

    FilterByListingDate edited

--- src/main/java/business/concretes/FilterByListingDate.java
+++ src/main/java/business/concretes/FilterByListingDate.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.WebDriver;
 public class FilterByListingDate extends PageCommon {
 
     By filterByListingDateLocator=By.cssSelector("a[data-id=date][title='Son 24 saat']");
-
     By submitButtonLocator=By.cssSelector("btn btn-block search-submit");
 
     public FilterByListingDate(WebDriver driver){
